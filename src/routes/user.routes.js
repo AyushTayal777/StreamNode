ec08e654586095485c03999c2ae3e00a8d9ba052
Ayush Tayal <ayushtayal685@gmail.com>
2025-08-08 17:28:12 +0530

    Implement JWT authentication and token management in user controller and routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import {registerUser} from "../controllers/user.controllers.js"
+import {loginUser, logoutUser, registerUser,refreshAccessToken} from "../controllers/user.controllers.js"
 import {upload} from "../middlewares/multer.middlewares.js"
-
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 const router = Router()
 
 router.route("/register").post(
@@ -17,6 +17,12 @@ router.route("/register").post(
 
     ]),
     registerUser)
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(verifyJWT,logoutUser)
+
+router.route("/refresh-token").post()
 
 export default router
 
